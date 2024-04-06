56e117d9fb5935651bb74ccb842b201fdfe54172
tnbe21 <whpbj809@gmail.com>
2024-04-07 03:30:52 +0900

    implement next button

--- src/App.js
+++ src/App.js
@@ -6,47 +6,13 @@ import {
 import client from './client';
 import { SEARCH_REPOSITORIES } from './graphql';
 
+const PER_PAGE = 5;
 const DEFAULT_STATE = {
   after: null,
   before: null,
-  first: 5,
+  first: PER_PAGE,
   last: null,
   query: 'フロントエンドエンジニア',
-};
-
-const Query = (props) => {
-  const { after, before, first, last, query } = props.state;
-  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
-    variables: { after, before, first, last, query },
-  });
-  if (loading) {
-    return 'Loading...';
-  }
-  if (error) {
-    return `Error! ${error.message}`;
-  }
-  
-  const search = data.search;
-  const repositoryCount = search.repositoryCount;
-  const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
-  const title = `Github Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
-  return (
-    <React.Fragment>
-      <h2>{title}</h2>
-      <ul>
-        {
-          search.edges.map(edge => {
-            const node = edge.node;
-            return (
-              <li key={node.id}>
-                <a href={node.url} rel='noreferrer' target='_blank'>{node.name}</a>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </React.Fragment>
-  );
 };
 
 class App extends Component {
@@ -70,7 +36,62 @@ class App extends Component {
     event.preventDefault();
   }
 
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null,
+    });
+  }
+
   render() {
+    const Query = (props) => {
+      const { after, before, first, last, query } = props.state;
+      const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+        variables: { after, before, first, last, query },
+      });
+      if (loading) {
+        return 'Loading...';
+      }
+      if (error) {
+        return `Error! ${error.message}`;
+      }
+
+      const search = data.search;
+      const repositoryCount = search.repositoryCount;
+      const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
+      const title = `Github Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
+      return (
+        <React.Fragment>
+          <h2>{title}</h2>
+          <ul>
+            {
+              search.edges.map(edge => {
+                const node = edge.node;
+                return (
+                  <li key={node.id}>
+                    <a href={node.url} rel='noreferrer' target='_blank'>{node.name}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+
+          {
+            search.pageInfo.hasNextPage === true ?
+              <button
+                onClick={this.goNext.bind(this, search)}
+              >
+                Next
+              </button>
+              :
+              null
+          }
+        </React.Fragment>
+      );
+    };
+
     const { query } = this.state;
     console.log({ query });
 
