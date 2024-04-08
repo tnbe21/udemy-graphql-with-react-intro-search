ff7c4d9f7a2f0e70ff9743fa9127cebc50e596b5
tnbe21 <whpbj809@gmail.com>
2024-04-09 00:09:50 +0900

    show viewerHasStarred state

--- src/App.js
+++ src/App.js
@@ -56,8 +56,15 @@ class App extends Component {
 
   render() {
     const StarButton = (props) => {
-      const totalCount = props.node.stargazers.totalCount;
-      return <button>{totalCount === 1 ? '1 star' : `${totalCount} stars`}</button>;
+      const node = props.node;
+      const totalCount = node.stargazers.totalCount;
+      const viewerHasStarred = node.viewerHasStarred;
+      const starCount = totalCount === 1 ? '1 star' : `${totalCount} stars`;
+      return (
+        <button>
+          {starCount} | {viewerHasStarred ? 'starred' : '-'}
+        </button>
+      );
     };
 
     const Query = (props) => {
